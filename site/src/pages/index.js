9efcbf1f23c8212e8195125364cbc5c415994941
Axel Mendoza <axel.mendoza@epita.fr>
2026-09-04 17:38:26 -0600

    feat: force dark theme on landing page only

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,12 +1,29 @@
+import {useEffect} from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import {useColorMode} from '@docusaurus/theme-common';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageLifecycle from '@site/src/components/HomepageLifecycle';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+
+function ForceDarkMode() {
+  const {colorMode, setColorMode} = useColorMode();
+  useEffect(() => {
+    const previousMode = colorMode;
+    setColorMode('dark');
+    document.body.classList.add('homepage-dark');
+    return () => {
+      setColorMode(previousMode);
+      document.body.classList.remove('homepage-dark');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return null;
+}
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -35,6 +52,7 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
+      <ForceDarkMode />
       <HomepageHeader />
       <main>
         <HomepageFeatures />
