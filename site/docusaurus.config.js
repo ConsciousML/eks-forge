e7196120497892d6b91393dbc9267e52924a2de3
Axel Mendoza <axel.mendoza@epita.fr>
2026-09-04 17:26:33 -0600

    feat: use Agdasima font for hero and navbar title

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -28,6 +28,31 @@ const config = {
 
   onBrokenLinks: 'throw',
 
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: 'anonymous',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&display=swap',
+      },
+    },
+  ],
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
