4680638544b9f613491e4be3a49620e0c40e6b8a
Axel Mendoza <axel.mendoza@epita.fr>
2026-09-07 16:43:59 -0600

    style: switch theme accent color to blue

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Welcome to ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <ForceDarkMode />
       <HomepageHeader />
