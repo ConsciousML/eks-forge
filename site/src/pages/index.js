c9b46079b041ee81e4339a3db98185b3f2a19f78
Axel Mendoza <axel.mendoza@epita.fr>
2026-09-08 11:06:51 -0600

    fix: link get started button to quickstart

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -44,7 +44,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/overview">
+            to="/docs/quickstart">
             Get Started
           </Link>
         </div>
